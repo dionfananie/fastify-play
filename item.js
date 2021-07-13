569f8e31faf03a3385a641ad9cb582e19264a09b
dion fananie <dionfananie@gmail.com>
2021-07-13 07:05:55 +0700

    fix code

--- item.js
+++ item.js
@@ -1,7 +1,7 @@
-let items = [
-    {id:'1', name: 'dion'},
-    {id:'2', name: 'edo'},
-    {id:'3', name: 'fananie'},
-]
+const items = [
+    { id: '1', name: 'dion' },
+    { id: '2', name: 'edo' },
+    { id: '3', name: 'fananie' }
+];
 
-module.exports = items
+module.exports = items;
--- server.js
+++ server.js
@@ -2,28 +2,28 @@
 const PORT = 3000;
 
 const fastify = require('fastify')({
-  logger: true
+    logger: true
 });
 
-fastify.register(require('fastify-swagger'),{
-  exposeRoute:true,
-  routePrefix:'/docs',
-  swagger:{
-    info:{
-      title: 'fastify-api'
+fastify.register(require('fastify-swagger'), {
+    exposeRoute: true,
+    routePrefix: '/docs',
+    swagger: {
+        info: {
+            title: 'fastify-api'
+        }
     }
-  }
-})
+});
 
-fastify.register(require('./routes/items'))
+fastify.register(require('./routes/items'));
 
-const start =  async()=>{
-  try {
-    await fastify.listen(PORT)
-  } catch (error) {
-    fastify.log.error(error);
-    process.exit(1)
-  }
-}
+const start = async () => {
+    try {
+        await fastify.listen(PORT);
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
 
-start()
+start();
