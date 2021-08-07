8713c4d437112a33ba375f884db2d9052c2038a9
dion fananie <dionfananie@gmail.com>
2021-08-07 13:24:59 +0700

    separate file dev

---
+++ server-dev.js
@@ -0,0 +1,29 @@
+// Require the framework and instantiate it
+const PORT = 3000;
+
+const fastify = require('fastify')({
+    logger: true
+});
+
+fastify.register(require('fastify-swagger'), {
+    exposeRoute: true,
+    routePrefix: '/docs',
+    swagger: {
+        info: {
+            title: 'fastify-api'
+        }
+    }
+});
+
+fastify.register(require('./routes/items'));
+
+const start = async () => {
+    try {
+        await fastify.listen(PORT);
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
+
+start();
